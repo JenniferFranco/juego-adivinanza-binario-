74672e1be2b21d6b58054a32174d8eb98d2fbd8f
Jennifer Franco <jennyfranco31.jf@gmail.com>
2025-04-15 12:05:54 -0300

    Base inicial del TP: variables y estructura de funciones definidas

---
+++ juego_binario.py
@@ -0,0 +1,25 @@
+#librerias
+import random;
+
+#desarrollo de funciones
+
+# def mostrar_mensajes_iniciales (): #mensaje de bienvenido al juego y de ingrese número 
+
+# def generar_numero_secreto ():  #usar math para generar un número al asar
+    
+# def transformar_a_binario (): #transformar el número secreto a binario
+    
+# def verificar_intento (): #comparar el número secreto y darle ayuda al usuario (si el num es mayor o menor) 
+    
+            
+#programa principal
+print('Juego de Adivinanza en Binario');
+print('Convertí el siguiente número binario a decimal:')
+num_secreto = random.randint(1,10); #establecer limite
+print(num_secreto)
+
+num = int(input('¿Qué número crees que es?: ')); 
+
+#transformar_a_binario ()
+
+#verificar_intento ()
