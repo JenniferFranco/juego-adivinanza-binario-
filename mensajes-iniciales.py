c6a7bbaee5c6f2f75282a2083bd5283d0aae1c01
Diana Falla <dfalla1211@gmail.com>
2025-04-16 20:15:34 -0300

    Modificacion rama Diana

--- mensajes-iniciales.py
+++ mensajes-iniciales.py
@@ -14,13 +14,6 @@ def mostrar_mensajes_iniciales():
     print(f"{Fore.GREEN}¡Bienvenido al juego!{Style.RESET_ALL}")
     numero = input(f"{Fore.BLUE}Por favor, ingresa un número: {Style.RESET_ALL}")
     
-    # Validar que el input sea un número entero
-    while not numero.isdigit():
-        print(f"{Fore.RED}Error: Debes ingresar un número entero.{Style.RESET_ALL}")
-        numero = input(f"{Fore.BLUE}Por favor, ingresa un número: {Style.RESET_ALL}")
-
-    #Una vez que tenemos un número válido, lo convertimos a entero utilizando int() y lo retornamos
-    return int(numero)
 
 #llamamos a la función y mostramos el número ingresado por el usuario
 numero_ingresado = mostrar_mensajes_iniciales()
