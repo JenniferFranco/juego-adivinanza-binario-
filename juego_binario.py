69cba29cab8f34ce4fac56fbd9b7049fd5de2ab9
Claudio482 <claudio80.cf@gmail.com>
2025-04-18 14:41:49 -0300

    Guardo mis cambios locales antes de hacer pull

--- juego_binario.py
+++ juego_binario.py
@@ -1,20 +1,27 @@
-#librerias
+# librerias
 import random
 from colorama import init, Fore, Style
+init()
+# desarrollo de funciones
 
-#desarrollo de funciones
+# mensaje de bienvenido al juego e indicaciones
 
-#mensaje de bienvenido al juego e indicaciones
+
 def mostrar_mensajes_iniciales():
     print(f"{Fore.GREEN}¡Bienvenido al Juego de Adivinanza en Binario!{Style.RESET_ALL}")
-    print(f"Convertí el siguiente número {Fore.GREEN}binario{Style.RESET_ALL} a {Fore.BLUE}decimal{Style.RESET_ALL}:")
+    print(
+        f"Convertí el siguiente número {Fore.GREEN}binario{Style.RESET_ALL} a {Fore.BLUE}decimal{Style.RESET_ALL}:")
 
-#generar un número decimal al asar 
-def generar_numero_secreto ():  
-        num_secreto = random.randint(1,10); #establecer limite
-        return num_secreto   
-    
-#transformar el número generado en decimal a binario
+# generar un número decimal al asar
+
+
+def generar_numero_secreto():
+    num_secreto = random.randint(1, 10)  # establecer limite
+    return num_secreto
+
+# transformar el número generado en decimal a binario
+
+
 def transformar_a_binario(numero_ingresado):
     num_binario = ""  # se inicializa una cadena vacia para ir insertando los números
     dividendo = numero_ingresado
@@ -28,19 +35,22 @@ def transformar_a_binario(numero_ingresado):
         dividendo = dividendo // 2
     return num_binario
 
-#comparar el número secreto y darle ayuda al usuario (si el num es mayor o menor) 
-def verificar_intento (num_decimal): 
+# comparar el número secreto y darle ayuda al usuario (si el num es mayor o menor)
+
+
+def verificar_intento(num_decimal):
     num_usuario = 0
-    while num_decimal != num_usuario: #camparamos el número ingresado por el usurario con el generado con random
+    while num_decimal != num_usuario:  # camparamos el número ingresado por el usurario con el generado con random
         num_usuario = int(input('¿Qué número crees que es?: '))
         if num_usuario == num_decimal:
             print(f'{Fore.GREEN}¡Acertaste!{Style.RESET_ALL}')
-        elif num_usuario > num_decimal:    #ayuda al usuario si el número es menor 
+        elif num_usuario > num_decimal:  # ayuda al usuario si el número es menor
             print(f'{Fore.RED}Incorrecto{Style.RESET_ALL}, el número es menor: ')
-        else: #en caso contrario, ayuda al usuario si el número es mayor
-            print(f'{Fore.RED}Incorrecto{Style.RESET_ALL}, el número es mayor: ')    
-            
-#programa principal
+        else:  # en caso contrario, ayuda al usuario si el número es mayor
+            print(f'{Fore.RED}Incorrecto{Style.RESET_ALL}, el número es mayor: ')
+
+
+# programa principal
 mostrar_mensajes_iniciales()
 numero_decimal = generar_numero_secreto()
 numero_binario = transformar_a_binario(numero_decimal)
